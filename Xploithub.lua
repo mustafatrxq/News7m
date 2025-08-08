efac5e3d9fbaf1ca57db411db1e2783bd7ef1bc4
mustafatrxq <hflow7285@gmail.com>
2025-08-08 16:51:03 +0300

    Update Xploithub.lua
    mu
    
    Signed-off-by: mustafatrxq <hflow7285@gmail.com>

--- Xploithub.lua
+++ Xploithub.lua
@@ -1,75 +1,77 @@
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/Y0dp/R7/refs/heads/main/TT.Lua")))()
 
-local PlayersLocal = Players.LocalPlayer
-local greeting = "منوّر السكربت"
+-- تحديد الجملة حسب جنس اللاعب
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local greeting = "منوّر السكربت" -- افتراضي ولد
 
-if PlayersLocal and PlayersLocal.Character and PlayersLocal.Character:FindFirstChildOfClass("Shirt") then
-    local shirtId = PlayersLocal.Character:FindFirstChildOfClass("Shirt").ShirtTemplate
-    if shirtId and shirtId:lower():find("girl") then
-        greeting = "منوّرة السكربت"
-    end
+if LocalPlayer and LocalPlayer.Character and LocalPlayer.Character:FindFirstChildOfClass("Shirt") then
+local shirtId = LocalPlayer.Character:FindFirstChildOfClass("Shirt").ShirtTemplate
+if shirtId and shirtId:lower():find("girl") then
+greeting = "منوّرة السكربت" -- إذا كان لبس بنت
+end
 end
 
 MakeWindow({
-  Hub = {
-    Title = "Xploit ساموراي V0.1",
-    Animation = greeting
-  },
-  Key = {
-    KeySystem = false,
-    Title = "Key System",
-    Description = "",
-    KeyLink = "",
-    Keys = {"1234"},
-    Notifi = {
-      Notifications = true,
-      CorrectKey = "Running the Script...",
-      Incorrectkey = "The key is incorrect",
-      CopyKeyLink = "Copied to Clipboard"
-    }
-  }
+Hub = {
+Title = "Xploit  ساموراي  V0.1",
+Animation = greeting
+},
+Key = {
+KeySystem = false,
+Title = "Key System",
+Description = "",
+KeyLink = "",
+Keys = {"1234"},
+Notifi = {
+Notifications = true,
+CorrectKey = "Running the Script...",
+Incorrectkey = "The key is incorrect",
+CopyKeyLink = "Copied to Clipboard"
+}
+}
 })
 
 MinimizeButton({
-  Image = "rbxassetid://88122625843089",
-  Size = {40, 40},
-  Color = Color3.fromRGB(255, 125, 0),
-  Corner = true,
-  Stroke = false,
-  StrokeColor = Color3.fromRGB(255, 125, 0)
+Image = "rbxassetid://88122625843089",
+Size = {40, 40},
+Color = Color3.fromRGB(255, 125, 0),
+Corner = true,
+Stroke = false,
+StrokeColor = Color3.fromRGB(255, 125, 0)
 })
 
 local Main = MakeTab({
-    Name = "المعلومات",
-    Image = "rbxassetid://88122625843089",
-    TabTitle = false
+Name = "المعلومات",
+Image = "rbxassetid://88122625843089",
+TabTitle = false
 })
 
-AddImageLabel(Main, {
-  Name = "قناة السكربت",
-  Image = "rbxassetid://88122625843089"
+local Image = AddImageLabel(Main, {
+Name = "قناة السكربت",
+Image = "rbxassetid://88122625843089"
 })
 
 AddButton(Main, {
-  Name = "انضم",
-  Callback = function()
-    setclipboard('https://t.me/Prov_development')
-  end
+Name = "انضم",
+Callback = function()
+setclipboard('https://t.me/Prov_development')
+end
 })
 
 local Label = AddTextLabel(Main,
- "User: " .. PlayersLocal.Name .. "\n" ..
- "Map: " .. game.PlaceId .. "\n" ..
- "Time: " .. os.date("%H:%M:%S")
+"User: " .. LocalPlayer.Name .. "\n" ..
+"Map: " .. game.PlaceId .. "\n" ..
+"Time: " .. os.date("%H:%M:%S")
 )
 
 task.spawn(function()
- while true do
-  task.wait(1)
-  Label.Update(
-   "User: " .. PlayersLocal.Name .. "\n" ..
-   "Map: " .. game.PlaceId .. "\n" ..
-   "Time: " .. os.date("%H:%M:%S")
-  )
- end
+while true do
+task.wait(1)
+Label.Update(
+"User: " .. LocalPlayer.Name .. "\n" ..
+"Map: " .. game.PlaceId .. "\n" ..
+"Time: " .. os.date("%H:%M:%S")
+)
+end
 end)
