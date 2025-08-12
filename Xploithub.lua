4db81c080d8c178aba369e2caf7f6f23f3aed5fe
mustafatrxq <hflow7285@gmail.com>
2025-08-12 06:14:08 +0300

    Update Xploithub.lua
    z
    
    Signed-off-by: mustafatrxq <hflow7285@gmail.com>

--- Xploithub.lua
+++ Xploithub.lua
@@ -1,17 +1,106 @@
+مكتبة واجهة المستخدم
 local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/tbao143/Library-ui/refs/heads/main/Redzhubui"))()
-
+نافذة
 local Window = redzlib:MakeWindow({
-    Title = "Xploit Hub ‖ Brookhaven || V0.1",
-    SubTitle = "ساموراي - عركـ",
-    SaveFolder = "Xploithub"  -- لاحظت آخر مرة وضعت .lua هنا، حذفتها عشان تكون مجلد وليس ملف
+  Title = "redz Hub : Blox Fruits",
+  SubTitle = "by redz9999",
+  SaveFolder = "testando | redz lib v5.lua"
 })
+رمز
+Window:AddMinimizeButton({
+    Button = { Image = "rbxassetid://71014873973869", BackgroundTransparency = 0 },
+    Corner = { CornerRadius = UDim.new(35, 1) },
+})
+دعوة ديسكورد
+Tab1:AddDiscordInvite({
+    Name = "Name Hub",
+    Description = "Join server",
+    Logo = "rbxassetid://18751483361",
+    Invite = "Link discord invite",
+})
+فاتورة غير مدفوعة
+local Tab1 = Window:MakeTab({"Um", "cherry"})
+تعيين الموضوع
+مظلم
 
-redzlib:SetTheme("Dark")
+  redzlib:SetTheme("Dark")
+أغمق
 
-local Tab1 = Window:MakeTab({"Main Tab", ""}) -- خلت الصورة فاضية مؤقتًا
+  redzlib:SetTheme("Darker")
+أرجواني
 
-Tab1:AddParagraph({"حقوق", "تم التطوير بواسطة المطور ساموراي - المطور عركـ"})
-
+  redzlib:SetTheme("Purple")
+علامة التبويب "ابدأ"
 Window:SelectTab(Tab1)
+قسم
+local Section = Tab1:AddSection({"Section"})
+فقرة
+local Paragraph = Tab1:AddParagraph({"Paragraph", "This is a Paragraph\nSecond Line"})
+الحوار
+  local Dialog = Window:Dialog({
+    Title = "Dialog",
+    Text = "This is a Dialog",
+    Options = {
+      {"Confirm", function()
+        
+      end},
+      {"Maybe", function()
+        
+      end},
+      {"Cancel", function()
+        
+      end}
+    }
+  })
+زر
+Tab1:AddButton({"Print", function(Value)
+print("Hello World!")
+end})
+تبديل 1
+local Toggle1 = Tab1:AddToggle({
+  Name = "Toggle",
+  Description = "This is a <font color='rgb(88, 101, 242)'>Toggle</font> Example",
+  Default = false 
+})
+Toggle1:Callback(function(Value)
+ 
+end)
+تبديل 2
+Tab1:AddToggle({
+    Name = "Toggle",
+    Default = false,
+    Callback = function(v)
 
-print("واجهة redzlib تم تحميلها بنجاح")
+    end
+})
+منزلقات
+Tab1:AddSlider({
+  Name = "Speed",
+  Min = 1,
+  Max = 100,
+  Increase = 1,
+  Default = 16,
+  Callback = function(Value)
+  
+  end
+})
+اسقاط
+local Dropdown = Tab1:AddDropdown({
+  Name = "Players List",
+  Description = "Select the <font color='rgb(88, 101, 242)'>Number</font>",
+  Options = {"one", "two", "three"},
+  Default = "two",
+  Flag = "dropdown teste",
+  Callback = function(Value)
+    
+  end
+})
+مربع النص
+Tab1:AddTextBox({
+  Name = "Name item",
+  Description = "1 Item on 1 Server", 
+  PlaceholderText = "item only",
+  Callback = function(Value)
+    
+  end
+})
