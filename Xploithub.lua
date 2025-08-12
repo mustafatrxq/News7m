c7e65df48a3a915d3aaf33ae7841e5c14247be9b
mustafatrxq <hflow7285@gmail.com>
2025-08-12 09:03:48 +0300

    Update Xploithub.lua
    Zambia 
    
    Signed-off-by: mustafatrxq <hflow7285@gmail.com>

--- Xploithub.lua
+++ Xploithub.lua
@@ -1,104 +1,23 @@
-local redzlib = loadstring(game:HttpGet("https:/
-raw.githubusercontent.com/tbao143/Library-ui/refs/heads/main/Redzhubui"))()
+local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/tbao143/Library-ui/refs/heads/main/Redzhubui"))()
+
+redzlib:SetTheme("Dark")
 
 local Window = redzlib:MakeWindow({
-  Title = "redz Hub : Blox Fruits",
-  SubTitle = "by redz9999",
-  SaveFolder = "testando | AddMinimizeButton
+    Title = "redz Hub : Blox Fruits",
+    SubTitle = "by redz9999",
+    SaveFolder = "testando | redz lib v5.lua"
 })
 
 Window:AddMinimizeButton({
     Button = { Image = "rbxassetid://71014873973869", BackgroundTransparency = 0 },
-    Corner = { CornerRadius = UDim.new(35, 1) },
+    Corner = { CornerRadius = UDim.new(35, 1) }
 })
 
-Tab1:AddDiscordInvite({
-    Name = "Name Hub",
-    Description = "Join server",
-    Logo = "rbxassetid://18751483361",
-    Invite = "Link discord invite",
-})
-
-local Tab1 = Window:MakeTab({"Um", "cherry"})
-
-  redzlib:SetTheme("Dark")
-
-
-  redzlib:SetTheme("Darker")
-
-
-  redzlib:SetTheme("Purple")
-
+local Tab1 = Window:MakeTab({"ابدأ", "Start"})
 Window:SelectTab(Tab1)
 
-local Section = Tab1:AddSection({"Section"})
-
-local Paragraph = Tab1:AddParagraph({"Paragraph", "This is a Paragraph\nSecond Line"})
-
-  local Dialog = Window:Dialog({
-    Title = "Dialog",
-    Text = "This is a Dialog",
-    Options = {
-      {"Confirm", function()
-        
-      end},
-      {"Maybe", function()
-        
-      end},
-      {"Cancel", function()
-        
-      end}
-    }
-  })
-
-Tab1:AddButton({"Print", function(Value)
-print("Hello World!")
-end})
-
-local Toggle1 = Tab1:AddToggle({
-  Name = "Toggle",
-  Description = "This is a <font color='rgb(88, 101, 242)'>Toggle</font> Example",
-  Default = false 
-})
-Toggle1:Callback(function(Value)
- 
-end)
-
-Tab1:AddToggle({
-    Name = "Toggle",
-    Default = false,
-    Callback = function(v)
-
-    end
-})
-
-Tab1:AddSlider({
-  Name = "Speed",
-  Min = 1,
-  Max = 100,
-  Increase = 1,
-  Default = 16,
-  Callback = function(Value)
-  
-  end
-})
-
-local Dropdown = Tab1:AddDropdown({
-  Name = "Players List",
-  Description = "Select the <font color='rgb(88, 101, 242)'>Number</font>",
-  Options = {"one", "two", "three"},
-  Default = "two",
-  Flag = "dropdown teste",
-  Callback = function(Value)
-    
-  end
-})
-
-Tab1:AddTextBox({
-  Name = "Name item",
-  Description = "1 Item on 1 Server", 
-  PlaceholderText = "item only",
-  Callback = function(Value)
-    
-  end
+-- إضافة فقرة ترحيبية أو نص بسيط بدون أزرار أو عناصر تحكم
+Tab1:AddParagraph({
+    "مرحباً بك في redz Hub",
+    "هنا ستجد كل ما تحتاجه من سكربتات وأدوات للعبة Blox Fruits.\nتابعنا دائماً لأحدث التحديثات."
 })
