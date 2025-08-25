02cac1e3a2f6920de52e2aab5f003b8217f2aaf7
mustafatrxq <hflow7285@gmail.com>
2025-08-25 11:23:04 +0300

    Update extra.lua
    yu
    
    Signed-off-by: mustafatrxq <hflow7285@gmail.com>

--- extra.lua
+++ extra.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/R7KScript/usernaxo/refs/heads/main/GUI3")))()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/Y0dp/SCN/refs/heads/main/TT")))()
 
 MakeWindow({
   Hub = {
